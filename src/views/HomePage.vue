f270d187c9da08381316bc3d0e706c1c59dd9ae4
Luiz Gustavo Pompílio Diniz <lgpd@intranet.pitang.com>
2024-08-15 23:41:35 -0300

    Criando primeiras páginas

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,25 @@
 <template>
-    <h1>HOME</h1>
+    <b-navbar class="navbar" toggleable="lg" type="dark" variant="primary">
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+                <b-nav-item href="#">Docs</b-nav-item>
+                <b-nav-item href="#">Components</b-nav-item>
+                <b-nav-item href="#">Directives</b-nav-item>
+                <b-nav-item href="#">Icons</b-nav-item>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
 </template>
 
 <script>
+export default {
+    name: "TopBar",
+};
 </script>
+
+<style>
+.navbar {
+    background-color: #044c28 !important; 
+}
+</style>
